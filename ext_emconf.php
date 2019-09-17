58a5b401b35b1ac0880ac6e62b1ab5af6d47a840
Karoline Steinfatt <dieMelanie@gmail.com>
2019-09-17 18:30:18 +0200

    Update ext_emconf.php
    We tested this extension with the current verision of grid elements 9.3.0 and it worked fine.
    
    Grüße nach Monheim! Danke für die Extension!

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
-            'gridelements' => '9.2.0-9.2.99',
+            'gridelements' => '9.2.0-9.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
